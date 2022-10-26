6660aee7b06def925d78b862cbda4b504a7ec826
gkdfo40 <gkdfo40@gmail.com>
2022-10-26 15:44:00 +0900

    refactor: 드롭다운 페이지 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
+import styled from "styled-components";
+import DropDown from "./pages/DropDownPage";
+
 const App = () => {
-  return <div>Helow Wolrd</div>;
+  return (
+    <AppContainer>
+      <DropDown />
+    </AppContainer>
+  );
 };
 
 export default App;
+const AppContainer = styled.div`
+  flex-wrap: wrap;
+  display: flex;
+  justify-content: center;
+  padding: 20px 0;
+  gap: 10px;
+`;
